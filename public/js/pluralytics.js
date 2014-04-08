2c84a106eb1f2eda84777917d7aa36d1898309bb
Jim Skerritt <jrskerritt@gmail.com>
2014-04-07 21:44:00 -0400

    client js cleanup and refactoring

--- public/js/pluralytics.js
+++ public/js/pluralytics.js
@@ -1,92 +1,107 @@
-var socket = io.connect('/');
-var history = [];
-var maxHistory = 720;
-
 $(function() {
+    var socket = io.connect('/'),
+        history = [],
+        maxHistory = 720,
+        requestInterval = 2500,
+        siteList = $('.main ul'),
+        siteTemplate = _.template('<li><site><%= name %></site><traffic><%= traffic %></traffic></li>'),
+        graphTemplate = _.template('<min><%= chartMin %></min><max><%= chartMax %></max><svg width="100%" height="150" viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none"><path d="<%= d %>" fill="#53a63a" stroke="none" stroke-width="0" /></svg>');
+
     socket.on('traffic-res', function(res) {
         refresh(res);
     });
 
     setInterval(function() {
         getTrafficUpdate();
-    }, 2500);
+    }, requestInterval);
+
+
+
+    function getTrafficUpdate() {
+        socket.emit('traffic-req');
+    }
+
+    function refresh(trafficData) {
+        var totalTraffic = 0;
+
+        for (var siteName in trafficData) {
+            var traffic = trafficData[siteName],
+                li = siteList.find('li:has(site:contains(' + siteName + '))');
+            if (li[0])
+                updateSite(li, traffic);
+            else
+                addSite(siteName, traffic);
+            totalTraffic += +traffic;
+        }
+
+        updateTotalTraffic(totalTraffic);
+        drawGraph();
+    }
+
+    function addSite(name, traffic) {
+        siteList.append(siteTemplate({name: name, traffic: traffic}));
+    }
+
+    function updateSite(li, traffic) {
+        li.find('traffic').html(traffic);
+    }
+
+    function updateTotalTraffic(totalTraffic) {
+        var changeClass = getTrafficChangeDirection(totalTraffic);
+        $('h1').removeClass('up down')
+               .addClass(changeClass)
+               .html(totalTraffic);
+        updateHistory(totalTraffic);
+    }
+
+    function updateHistory(totalTraffic) {
+        history.push(totalTraffic);
+        if (history.length > maxHistory)
+            history.shift();
+    }
+
+    function getMostRecentHistory() {
+        if (history.length) {
+            return history[history.length - 1];
+        }
+    }
+
+    function getTrafficChangeDirection(totalTraffic) {
+        var mostRecentValue = getMostRecentHistory();
+        if (mostRecentValue) {
+            if (totalTraffic > mostRecentValue)
+                return 'up';
+            else if (totalTraffic < mostRecentValue)
+                return 'down';
+        }
+        return '';
+    }
+
+    function drawGraph() {
+        var d = 'M 0 150',
+            historyMaxIndex = history.length - 1,
+            columnWidth = (historyMaxIndex === 0) ? 0 : 150 / historyMaxIndex;
+
+        var chartMax = Math.ceil(history.max() / 50) * 50,
+            chartMin = Math.floor(history.min() / 50) * 50,
+            delta = chartMax - chartMin,
+            chartZero = (chartMin > delta / 10) ? chartMin - delta / 10 : 0,
+            chartRange = chartMax - chartZero,
+            r = (chartRange === 0) ? 0 : 150 / chartRange;
+
+        for (i = 0; i < history.length; i++) {
+            d += ' L ' + i * columnWidth + ' ' + (150 - (history[i] - chartZero) * r);
+        }
+        d += ' L 150 150 z';
+
+        $('.chart').html(graphTemplate({chartMin: chartMin, chartMax: chartMax, d: d}));
+    }
+
+    Array.prototype.min = function() {
+        return Math.min.apply(null, this);
+    };
+
+    Array.prototype.max = function() {
+        return Math.max.apply(null, this);
+    };
 });
-
-function getTrafficUpdate() {
-    socket.emit('traffic-req');
-}
-
-function refresh(trafficData) {
-    var list = $('.main ul'),
-        totalTraffic = 0;
-
-    list.empty();
-    for (var site in trafficData) {
-        appendSiteToList(list, trafficData, site);
-        totalTraffic += +trafficData[site];
-    }
-
-    updateTotalTraffic(totalTraffic);
-    drawSvg();
-}
-
-
-function appendSiteToList(list, trafficData, site) {
-    var listItem = $('.template').children().first().clone();
-    listItem.find('site').html(site);
-    listItem.find('traffic').html(trafficData[site]);
-    list.append(listItem);
-}
-
-function updateTotalTraffic(totalTraffic) {
-    var changeClass = '';
-
-    var mostRecentValue = getMostRecentHistory();
-    if (mostRecentValue) {
-        if (totalTraffic > mostRecentValue)
-            changeClass = 'up';
-        else if (totalTraffic < mostRecentValue)
-            changeClass = 'down';
-    }
-
-    $('h1').remove();
-    $('body').prepend('<h1 class="' + changeClass + '">' + totalTraffic + '</h1>');
-
-    history.push(totalTraffic);
-    if (history.length > maxHistory)
-        history.shift();
-}
-
-function drawSvg() {
-    var d = 'M 0 150',
-        historyMaxIndex = history.length - 1,
-        columnWidth = (historyMaxIndex === 0) ? 0 : 150 / historyMaxIndex;
-
-    var chartMax = Math.ceil(history.max() / 50) * 50,
-        chartMin = Math.floor(history.min() / 50) * 50,
-        delta = chartMax - chartMin,
-        chartZero = (chartMin > delta / 10) ? chartMin - delta / 10 : 0,
-        chartRange = chartMax - chartZero,
-        r = (chartRange === 0) ? 0 : 150 / chartRange;
-
-    for (i = 0; i < history.length; i++) {
-        d += ' L ' + i * columnWidth + ' ' + (150 - (history[i] - chartZero) * r);
-    }
-    d += ' L 150 150 z';
-
-    $('.chart').html('<min>' + chartMin + '</min><max>' + chartMax + '</max><svg width="100%" height="150" viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg"  preserveAspectRatio="none"><path d="' + d + '" fill="#53a63a" stroke="none" stroke-width="0" /></svg>');
-}
-
-function getMostRecentHistory() {
-    if (history.length) {
-        return history[history.length - 1];
-    }
-}
-
-Array.prototype.min = function() {
-    return Math.min.apply(null, this);
-};
-
-Array.prototype.max = function() {
-    return Math.max.apply(null, this);
-};
